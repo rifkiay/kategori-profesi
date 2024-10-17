46d2e298d71bd849ab772abe9df961b30d3df3cd
unknown <rifkiainul422@gmail.com>
2024-10-17 19:54:34 +0700

    change migration user to users

--- database/migrations/2024_10_13_075836_create_users_table.php
+++ database/migrations/2024_10_13_075836_create_users_table.php
@@ -17,11 +17,28 @@ return new class extends Migration
             $table->string('nama'); 
             $table->string('username'); 
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('minat');
             $table->text('bio');
             $table->text('foto_profile')->nullable();
+            $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -31,5 +48,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('user');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
