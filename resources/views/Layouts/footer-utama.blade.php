789e5baf8dbb7db81ac0cb5735305b8ad7a32a68
wildaaa12 <134575949+wildaaa12@users.noreply.github.com>
2024-10-28 14:27:12 +0700

    Menambah layouts footer utama

---
+++ resources/views/Layouts/footer-utama.blade.php
@@ -0,0 +1,65 @@
+<!-- Footer Start -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <!-- Logo dan Deskripsi -->
+            <div class="col-lg-3 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
+                <a href="{{ url('/') }}"><img src="{{ asset('Assets/images/logo-light.png') }}" height="20" alt="Logo"></a>
+                <p class="mt-4">Temukan informasi lengkap mengenai berbagai profesi untuk membantu Anda memilih karir yang tepat.</p>
+                <ul class="social-icon social list-inline mb-0">
+                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-linkedin"></i></a></li>
+                </ul>
+            </div>
+
+            <!-- Informasi Perusahaan -->
+            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                <p class="text-white mb-4 footer-list-title">Perusahaan</p>
+                <ul class="list-unstyled footer-list">
+                    <li><a href="{{ url('about') }}" class="text-foot"><i class="mdi mdi-chevron-right"></i> Tentang Kami</a></li>
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Karir</a></li>
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Blog</a></li>
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Loker</a></li>
+                </ul>
+            </div>
+
+            <!-- Sumber Daya -->
+            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                <p class="text-white mb-4 footer-list-title">Sumber Daya</p>
+                <ul class="list-unstyled footer-list">
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Dukungan</a></li>
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Kebijakan Privasi</a></li>
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Syarat & Ketentuan</a></li>
+                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> FAQ</a></li>
+                </ul>
+            </div>
+
+            <!-- Jam Operasional -->
+            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                <p class="text-white mb-4 footer-list-title">Jam Operasional</p>
+                <ul class="list-unstyled text-foot mt-4 mb-0">
+                    <li>Senin - Jumat : 09:00 - 17:00</li>
+                    <li class="mt-2">Sabtu : 10:00 - 15:00</li>
+                    <li class="mt-2">Minggu : Libur</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<hr>
+
+<footer class="footer footer-bar">
+    <div class="container text-center">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="">
+                    <p class="mb-0">© 2019 -2020 Jobya. Design with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
