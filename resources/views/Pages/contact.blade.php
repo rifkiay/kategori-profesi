464db046ff117ee5a02c8b8f13f2cf14a33140cc
wildaaa12 <134575949+wildaaa12@users.noreply.github.com>
2024-11-11 14:35:19 +0700

    menambahkan halaman contact

---
+++ resources/views/Pages/contact.blade.php
@@ -0,0 +1,151 @@
+@include('layouts.navbar-utama')
+    
+    <!-- Header Section -->
+    <section class="bg-half page-next-level"> 
+        <div class="bg-overlay"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="text-center text-white">
+                        <h4 class="text-uppercase title mb-4">Contact Us</h4>
+                        <ul class="page-next d-inline-block mb-0">
+                            <li><a href="index.html" class="text-uppercase font-weight-bold">Home</a></li>
+                            <li><span class="text-uppercase text-white font-weight-bold">Contact Us</span></li> 
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Info Section -->
+    <section class="section pt-0 bg-light">
+        <div class="container mt-100 mt-60">
+            <div class="row align-items-center">
+                <div class="col-lg-4">
+                    <div class="contact-item mt-40">
+                        <div class="contact-icon d-inline-block border rounded-pill shadow text-primary mt-1 me-4">
+                            <i class="mdi mdi-earth"></i>
+                        </div>
+                        <div class="contact-details">
+                            <h4 class="text-dark mb-1">Website</h4>
+                            <p class="mb-0 text-muted">jobya.com</p>
+                            <p class="mb-0 text-muted">support@jobya.com</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="contact-item mt-40">
+                        <div class="contact-icon d-inline-block border rounded-pill shadow text-primary mt-1 me-4">
+                            <i class="mdi mdi-cellphone-iphone"></i>
+                        </div>
+                        <div class="contact-details">
+                            <h4 class="text-dark mb-1">Call us</h4>
+                            <p class="mb-0 text-muted">+1 800 123 456</p>
+                            <p class="mb-0 text-muted">+1 800 789 012</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="contact-item mt-40">
+                        <div class="contact-icon d-inline-block border rounded-pill shadow text-primary mt-1 me-4">
+                            <i class="mdi mdi-email"></i>
+                        </div>
+                        <div class="contact-details">
+                            <h4 class="text-dark mb-1">Email</h4>
+                            <p class="mb-0 text-muted">info@jobya.com</p>
+                            <p class="mb-0 text-muted">help@jobya.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Map Section -->
+    <section class="section bg-light">
+        <div class="container-fluid p-0">
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233667.82239218664!2d-118.69192185540902!3d34.020161306813886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c0be0b4a3fb1%3A0x1c72c7f9a7dfd9b1!2sLos+Angeles%2C+CA%2C+USA!5e0!3m2!1sen!2sus!4v1618264012548!5m2!1sen!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Form Section -->
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="text-dark mb-0">Get In Touch :</h4>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-8 col-md-7 mt-4 pt-2">
+                    <div class="custom-form rounded border p-4">
+                        <div id="message"></div>
+                        <form method="post" action="php/contact.php" name="contact-form" id="contact-form">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group app-label">
+                                        <label class="text-muted">Name</label>
+                                        <input name="name" id="name2" type="text" class="form-control resume" placeholder="Enter Name..">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group app-label">
+                                        <label class="text-muted">Email address</label>
+                                        <input name="email" id="email1" type="email" class="form-control resume" placeholder="Enter Email..">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group app-label">
+                                        <label class="text-muted">Subject</label>
+                                        <input type="text" class="form-control resume" id="subject" placeholder="Subject..">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group app-label">
+                                        <label class="text-muted">Message</label>
+                                        <textarea name="comments" id="comments" rows="5" class="form-control resume" placeholder="Message.."></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <input type="submit" id="submit" name="send" class="submitBnt btn btn-primary" value="Send Message">
+                                    <div id="simple-msg"></div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <!-- Contact Info Card -->
+                <div class="col-lg-4 col-md-5 mt-4 pt-2">
+                    <div class="border rounded text-center p-4">
+                        <h5 class="text-dark pb-3">Contact Info</h5>
+                        <div class="contact-location rounded mt-5 p-4">
+                            <div class="contact-location-icon bg-white text-primary rounded-pill">
+                                <i class="mdi mdi-map-marker"></i>
+                            </div>
+                            <p class="text-muted pt-4 f-20 mb-0">2453 Clinton Street, Little Rock, AR 72211</p>
+                        </div>
+                        <h6 class="text-muted mt-4 mb-0">Share</h6>
+                        <ul class="list-unstyled social-icon mt-3 mb-0">
+                            <li class="list-inline-item"><a href="#" class=""><i class="mdi mdi-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="mdi mdi-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="mdi mdi-whatsapp"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="mdi mdi-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class=""><i class="mdi mdi-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    @include('layouts.footer-utama')
+   
