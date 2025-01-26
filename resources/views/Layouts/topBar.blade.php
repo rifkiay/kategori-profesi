f9303e8b7b17bae65983f5f703633ac89b1fef6b
R.Angel <adis.anjeli01@gmail.com>
2025-01-26 10:12:08 +0700

    fix:mengubah search bar jadi nama kampus

--- resources/views/Layouts/topBar.blade.php
+++ resources/views/Layouts/topBar.blade.php
@@ -4,19 +4,19 @@
                 <div class="d-flex align-items-center gap-1">
 
                     <!-- Topbar Brand Logo -->
-                    <div class="logo-topbar">
-                        <!-- Logo light -->
-                        <a href="index.html" class="logo-light">
+                    <!-- <div class="logo-topbar"> -->
+                    <!-- Logo light -->
+                    <!-- <a href="index.html" class="logo-light">
                             <span class="logo-lg">
                                 <img src="assets/images/logo.png" alt="logo">
                             </span>
                             <span class="logo-sm">
                                 <img src="assets/images/logo-sm.png" alt="small logo">
                             </span>
-                        </a>
+                        </a> -->
 
-                        <!-- Logo Dark -->
-                        <a href="index.html" class="logo-dark">
+                    <!-- Logo Dark -->
+                    <!-- <a href="index.html" class="logo-dark">
                             <span class="logo-lg">
                                 <img src="assets/images/logo-dark.png" alt="dark logo">
                             </span>
@@ -24,52 +24,36 @@
                                 <img src="assets/images/logo-sm.png" alt="small logo">
                             </span>
                         </a>
-                    </div>
+                    </div> -->
 
                     <!-- Sidebar Menu Toggle Button -->
-                    <button class="button-toggle-menu">
+                    <!-- <button class="button-toggle-menu">
                         <i class="ri-menu-line"></i>
-                    </button>
+                    </button> -->
 
                     <!-- Horizontal Menu Toggle Button -->
-                    <button class="navbar-toggle" data-bs-toggle="collapse" data-bs-target="#topnav-menu-content">
+                    <!-- <button class="navbar-toggle" data-bs-toggle="collapse" data-bs-target="#topnav-menu-content">
                         <div class="lines">
                             <span></span>
                             <span></span>
                             <span></span>
                         </div>
-                    </button>
+                    </button> -->
 
                     <!-- Topbar Search Form -->
-                    <div class="app-search d-none d-lg-block">
-                        <form>
+                    <div class="app-search d-none d-lg-block mt-1 ms-2">
+                        <img src="{{ asset('Assets/images/logo-ars.png') }}" height="21" alt="University Logo" class="mb-1">
+                        <strong>ARS UNIVERSITY</strong>
+                        <!-- <form>
                             <div class="input-group">
                                 <input type="search" class="form-control" placeholder="cari...">
                                 <span class="ri-search-line search-icon text-muted"></span>
                             </div>
-                        </form>
+                        </form> -->
                     </div>
                 </div>
 
                 <ul class="topbar-menu d-flex align-items-center gap-3">
-                    <li class="dropdown d-lg-none">
-                        <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button"
-                            aria-haspopup="false" aria-expanded="false">
-                            <i class="ri-search-line fs-22"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-animated dropdown-lg p-0">
-                            <form class="p-3">
-                                <input type="search" class="form-control" placeholder="Cari ..."
-                                    aria-label="Recipient's username">
-                            </form>
-                        </div>
-                    </li>
-
-                    <li class="d-none d-sm-inline-block">
-                        <div class="nav-link" id="light-dark-mode">
-                            <i class="ri-moon-line fs-22"></i>
-                        </div>
-                    </li>
 
                     <li class="dropdown">
                         <a class="nav-link dropdown-toggle arrow-none nav-user" data-bs-toggle="dropdown" href="#" role="button"
@@ -83,10 +67,6 @@
                             </span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated profile-dropdown">
-                            <!-- item-->
-                            <div class=" dropdown-header noti-title">
-                                <h6 class="text-overflow m-0">Welcome !</h6>
-                            </div>
 
                             <!-- item-->
                             <a href="{{ url('/profile') }}" class="dropdown-item">
@@ -95,7 +75,7 @@
                             </a>
 
                             <!-- item-->
-                            <a href="#" class="dropdown-item" 
+                            <a href="#" class="dropdown-item"
                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 <i class="ri-logout-box-line fs-18 align-middle me-1"></i>
                                 <span>Logout</span>
