79a2378a182748848cc8db83e028253867b7ce04
R.Angel <adis.anjeli01@gmail.com>
2025-01-26 11:05:04 +0700

    fix:tampilan detail profesi tapi gambarnya belom

--- resources/views/Profesi/detail.blade.php
+++ resources/views/Profesi/detail.blade.php
@@ -23,10 +23,9 @@
                             <span class="picture-bg-overlay">
 
                             </span>
-                            <!-- overlay -->
                         </div>
                         <!-- meta -->
-                        <div class="profile-user-box">
+                        <!-- <div class="profile-user-box">
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="profile-user-img">
@@ -43,7 +42,7 @@
                                         </h5>
 
                                         <p class="text-muted mb-0 text-lg"><small> <span class="badge bg-success bg-opacity-13 p-2">
-                                                   Rp. {{$profesi->gaji}}
+                                                    Rp. {{$profesi->gaji}}
                                                 </span></small></p>
                                     </div>
                                 </div>
@@ -57,80 +56,84 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!--/ meta -->
+                        </div> -->
+                        <!--/ meta
                     </div>
                 </div>
-                <!-- end row -->
+                end row -->
 
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="card p-0">
-                            <div class="card-body p-0">
-                                <div class="profile-content">
-                                    <ul class="nav nav-underline nav-justified gap-0">
-                                        <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab"
-                                                data-bs-target="#aboutme" type="button" role="tab"
-                                                aria-controls="home" aria-selected="true" href="#aboutme">Deskripsi Profesi</a>
-                                        </li>
-
-
-
-                                    </ul>
-
-                                    <div class="tab-content m-0 p-4">
-                                        <div class="tab-pane active" id="aboutme" role="tabpanel"
-                                            aria-labelledby="home-tab" tabindex="0">
-                                            <div class="profile-desk">
-
-                                                <p class="text-muted fs-16">
-                                                    {{$profesi->deskripsi_profesi}}
-                                                </p>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card p-0">
+                                    <div class="card-body p-0">
+                                        <div class="profile-content">
+                                            <ul class="nav nav-underline nav-justified gap-0">
+                                                <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab"
+                                                        data-bs-target="#aboutme" type="button" role="tab"
+                                                        aria-controls="home" aria-selected="true" href="#aboutme">{{$profesi->nama_profesi}}</a>
+                                                </li>
 
 
-                                                <table class="table table-condensed mb-0 border-top">
-                                                    <tbody>
-                                                        <tr>
-                                                            <th scope="row">Perusahaan</th>
-                                                            <td>
-                                                                <a href="#" class="ng-binding">
-                                                                    JayaAtmaja
-                                                                </a>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row">Email</th>
-                                                            <td>
-                                                                <a href="" class="ng-binding">
-                                                                    HRDRec@example.com
-                                                                </a>
-                                                            </td>
-                                                        </tr>
 
-                                                        <tr>
-                                                            <th scope="row">Icon</th>
-                                                            <td class="ng-binding"> <i class="mdi mdi-heart-pulse d-inline-block rounded-pill h3 text-primary"></i></td>
-                                                        </tr>
+                                            </ul>
+
+                                            <div class="tab-content m-0 p-4">
+                                                <div class="tab-pane active" id="aboutme" role="tabpanel"
+                                                    aria-labelledby="home-tab" tabindex="0">
+                                                    <div class="profile-desk">
+                                                        <p class="text-muted fs-16 ">
+                                                        {{-- {{$profesi->deskripsi_profesi}} --}}
+                                                            {!! nl2br(e($profesi->deskripsi_profesi)) !!}
+                                                        </p>
 
 
-                                                    </tbody>
-                                                </table>
-                                            </div> <!-- end profile-desk -->
-                                        </div> <!-- about-me -->
+                                                        <table class="table table-condensed mb-0 border-top">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <th scope="row" style="padding-right: 10px; width: 150px;">Keterampilan</th>
+                                                                    <td>
+                                                                        <ul class="list-unstyled" >
+                                                                            @php
+                                                                            $keterampilan = explode("\n", $profesi->keterampilan); // Memisahkan berdasarkan newline
+                                                                            @endphp
+
+                                                                            @foreach ($keterampilan as $keterampilanItem)
+                                                                            <li><i class="mdi mdi-check-circle-outline text-success"></i> {{ $keterampilanItem }}</li>
+                                                                            @endforeach
+                                                                        </ul>
+                                                                    </td>
+                                                                </tr>
+
+                                                                <tr>
+                                                                    <th scope="row">Keterampilan</th>
+                                                                    <td>
+                                                                        Rp. {{$profesi->gaji}}
+
+                                                                    </td>
+                                                                </tr>
 
 
+
+
+
+                                                            </tbody>
+                                                        </table>
+                                                    </div> <!-- end profile-desk -->
+                                                </div> <!-- about-me -->
+
+
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <!-- end page title -->
                     </div>
+                    <!-- container -->
                 </div>
-                <!-- end page title -->
+                <!-- content -->
+                @include('Layouts.footer')
             </div>
-            <!-- container -->
         </div>
-        <!-- content -->
-        @include('Layouts.footer')
-    </div>
-</div>
-<!-- END wrapper -->
+        <!-- END wrapper -->
