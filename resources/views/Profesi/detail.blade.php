e3783e25d8421c5b1cf3540d9780272a5e85ffac
R.Angel <adis.anjeli01@gmail.com>
2025-01-28 17:25:31 +0700

    fix:id kategori profesi diubah jadi nama nya + fix tampilan detail dan tabel

--- resources/views/Profesi/detail.blade.php
+++ resources/views/Profesi/detail.blade.php
@@ -11,129 +11,87 @@
             <!-- Start Content-->
             <div class="container-fluid">
                 @section('namaPage1', 'Profesi')
-                @section('namaPage2', 'Detail Profesi')
+                @section('namaPage2', $profesi->nama_profesi)
                 @include('Layouts.breadcrumb')
 
                 <!-- Start profile -->
                 <!-- start page title -->
                 <div class="row">
                     <div class="col-sm-12">
-                        <div class="profile-bg-picture">
-                            <img src="{{ asset('Assets/images/bg-counters.jpg') }}" alt="Gambar Profesi" width="100%" height="auto">
+                        <div class="profile-bg-picture-profesi">
+                            <img src="{{ asset('storage/' . $profesi->gambar_profesi) }}" alt="Gambar Profesi" width="100%" height="auto">
                             <span class="picture-bg-overlay">
 
                             </span>
                         </div>
-                        <!-- meta -->
-                        <!-- <div class="profile-user-box">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="profile-user-img">
-                                        @if($profesi->gambar_profesi)
-                                        <img src="{{ asset('storage/' . $profesi->gambar_profesi) }}" alt="Gambar Profesi" width="100%" height="300" class="avatar-lg rounded-circle">
-                                        @else
-                                        <span>Gambar tidak tersedia</span>
-                                        @endif
-                                    </div>
-                                    <div class="">
-                                        <h4 class="mt-4 fs-17 ellipsis">{{ $profesi->nama_profesi }}</h4>
-                                        <h5>Keterampilan:
-                                            <p class="font-13">{{$profesi->keterampilan}}</p>
-                                        </h5>
-
-                                        <p class="text-muted mb-0 text-lg"><small> <span class="badge bg-success bg-opacity-13 p-2">
-                                                    Rp. {{$profesi->gaji}}
-                                                </span></small></p>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="d-flex justify-content-end align-items-center gap-2">
-                                        <a href="{{ route('profesi.edit', $profesi->id) }}" class="btn btn-soft-info">
-                                            <i class="ri-pencil-fill align-text-bottom me-1 fs-16 lh-1"></i>
-                                            Edit Profesi
-                                        </a>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div> -->
-                        <!--/ meta
                     </div>
                 </div>
-                end row -->
+                <!-- end row -->
 
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="card p-0">
-                                    <div class="card-body p-0">
-                                        <div class="profile-content">
-                                            <ul class="nav nav-underline nav-justified gap-0">
-                                                <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab"
-                                                        data-bs-target="#aboutme" type="button" role="tab"
-                                                        aria-controls="home" aria-selected="true" href="#aboutme">{{$profesi->nama_profesi}}</a>
-                                                </li>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card p-0">
+                            <div class="card-body p-0">
+                                <div class="profile-content">
+                                    <ul class="nav nav-underline nav-justified gap-0">
+                                        <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab"
+                                                data-bs-target="#aboutme" type="button" role="tab"
+                                                aria-controls="home" aria-selected="true" href="#aboutme">
+                                                <h1>{{$profesi->nama_profesi}}</h1>
+                                            </a>
+                                        </li>
+                                    </ul>
 
-
-
-                                            </ul>
-
-                                            <div class="tab-content m-0 p-4">
-                                                <div class="tab-pane active" id="aboutme" role="tabpanel"
-                                                    aria-labelledby="home-tab" tabindex="0">
-                                                    <div class="profile-desk">
-                                                        <p class="text-muted fs-16 ">
-                                                        {{-- {{$profesi->deskripsi_profesi}} --}}
-                                                            {!! nl2br(e($profesi->deskripsi_profesi)) !!}
-                                                        </p>
-
-
-                                                        <table class="table table-condensed mb-0 border-top">
-                                                            <tbody>
-                                                                <tr>
-                                                                    <th scope="row" style="padding-right: 10px; width: 150px;">Keterampilan</th>
-                                                                    <td>
-                                                                        <ul class="list-unstyled" >
-                                                                            @php
-                                                                            $keterampilan = explode("\n", $profesi->keterampilan); // Memisahkan berdasarkan newline
-                                                                            @endphp
-
-                                                                            @foreach ($keterampilan as $keterampilanItem)
-                                                                            <li><i class="mdi mdi-check-circle-outline text-success"></i> {{ $keterampilanItem }}</li>
-                                                                            @endforeach
-                                                                        </ul>
-                                                                    </td>
-                                                                </tr>
-
-                                                                <tr>
-                                                                    <th scope="row">Gaji</th>
-                                                                    <td>
-                                                                        Rp. {{$profesi->gaji}}
-
-                                                                    </td>
-                                                                </tr>
+                                    <div class="tab-content m-0 p-4">
+                                        <div class="tab-pane active" id="aboutme" role="tabpanel"
+                                            aria-labelledby="home-tab" tabindex="0">
+                                            <div class="profile-desk">
+                                                <p class="text-muted fs-16 ">
+                                                    {{-- {{$profesi->deskripsi_profesi}} --}}
+                                                    {!! nl2br(e($profesi->deskripsi_profesi)) !!}
+                                                </p>
 
 
+                                                <table class="table table-condensed mb-0 border-top">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th scope="row" style="padding-right: 10px; width: 150px;">Keterampilan</th>
+                                                            <td>
+                                                                <ul class="list-unstyled">
+                                                                    @php
+                                                                    $keterampilan = explode("\n", $profesi->keterampilan); // Memisahkan berdasarkan newline
+                                                                    @endphp
 
+                                                                    @foreach ($keterampilan as $keterampilanItem)
+                                                                    <li><i class="mdi mdi-check-circle-outline text-success"></i> {{ $keterampilanItem }}</li>
+                                                                    @endforeach
+                                                                </ul>
+                                                            </td>
+                                                        </tr>
 
+                                                        <tr>
+                                                            <th scope="row">Gaji</th>
+                                                            <td>
+                                                                Rp. {{$profesi->gaji}}
 
-                                                            </tbody>
-                                                        </table>
-                                                    </div> <!-- end profile-desk -->
-                                                </div> <!-- about-me -->
-
-
-                                            </div>
-                                        </div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div> <!-- end profile-desk -->
+                                        </div> <!-- about-me -->
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- end page title -->
                     </div>
-                    <!-- container -->
                 </div>
-                <!-- content -->
-                @include('Layouts.footer')
+                <!-- end page title -->
             </div>
+            <!-- container -->
         </div>
-        <!-- END wrapper -->
+        <!-- content -->
+        @include('Layouts.footer')
+    </div>
+</div>
+<!-- END wrapper -->
--- resources/views/Profesi/show.blade.php
+++ resources/views/Profesi/show.blade.php
@@ -34,62 +34,80 @@
 
                             <div class="card-body">
                                 <div class="table-responsive">
-                                <table id="scroll-horizontal-datatable" class="table table-striped w-100 nowrap">
-                                    <thead>
-                                        <tr class="text-center text-nowrap">
-                                            <th class="text-center">No</th>
-                                            <th class="text-center">ID Kategori Profesi</th>
-                                            <th class="text-center">Nama Profesi</th>
-                                            <th class="text-center" style="width: 40%;">Deskripsi Profesi</th>
-                                            <th class="text-center">Keterampilan</th>
-                                            <th class="text-center">Gaji</th>
-                                            <th class="text-center">Gambar Profesi</th>
-                                            <th class="text-center">Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody style="font-size: 0.7rem;">
-                                        @foreach ($profesi as $item)
-                                        <tr>
-                                            <td class="text-center align-middle">{{ $loop->iteration }}</td>
-                                            <td class="text-center align-middle">{{ $item->id_kategori_profesi }}</td>
-                                            <td class="text-center align-middle">{{ $item->nama_profesi }}</td>
-                                            <td class="align-middle text-wrap" style="text-align: justify; max-width: 200px;">{{ $item->deskripsi_profesi }}</td>
-                                            <td class="text-center align-middle">{{ $item->keterampilan }}</td>
-                                            <td class="text-center text-nowrap align-middle ">
-                                                <span class="badge bg-success bg-opacity-15 p-1">
-                                                    Rp {{$item->gaji}}
-                                                </span>
-                                            </td>
-                                            <td class="text-center">
-                                                @if($item->gambar_profesi)
-                                                <img src="{{ asset('storage/' . $item->gambar_profesi) }}" alt="Gambar Profesi" width="100">
-                                                @else
-                                                <span>Gambar tidak tersedia</span>
-                                                @endif
-                                            </td>
-                                            <td class="text-center">
-                                                <div class="d-flex justify-content-center align-items-center gap-1" style="min-height: 100px;">
-                                                <a href="{{ route('profesi.show', $item->id) }}" class="btn btn-warning btn-sm"><i class="ri-eye-fill"></i></a>
-                                                    <a href="{{ route('profesi.edit', $item->id) }}" class="btn btn-info btn-sm">
-                                                        <i class="ri-pencil-fill"></i>
-                                                    </a>
-                                                    <form action="{{ route('profesi.destroy', $item->id) }}" method="POST" style="display:inline;">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?');">
-                                                            <i class="ri-delete-bin-fill"></i>
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            </td>
+                                    <table id="scroll-horizontal-datatable" class="table table-striped w-100 nowrap">
+                                        <thead>
+                                            <tr class="text-center text-nowrap">
+                                                <th class="text-center">No</th>
+                                                <th class="text-center">Kategori Profesi</th>
+                                                <th class="text-center">Nama Profesi</th>
+                                                <th class="text-center" style="width: 40%;">Deskripsi Profesi</th>
+                                                <th class="text-center">Keterampilan</th>
+                                                <th class="text-center">Gaji</th>
+                                                <th class="text-center">Gambar Profesi</th>
+                                                <th class="text-center">Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody style="font-size: 0.8rem;">
+                                            @foreach ($profesi as $item)
+                                            <tr>
+                                                <td class="text-center align-middle">{{ $loop->iteration }}</td>
+                                                <td class="text-center align-middle">{{ $item->kategoriPerusahaan->kategori_profesi }}</td>
+                                                <td class="text-center align-middle">{{ $item->nama_profesi }}</td>
+                                                <td class="align-middle text-wrap" style="text-align: justify; max-width: 200px;">{{ Str::limit($item->deskripsi_profesi, 80) }}</td>
+                                                <td class=" align-middle">
+                                                    <ul class="list-unstyled">
+                                                        @php
+                                                        $keterampilanList = explode("\n", $item->keterampilan);
+                                                        @endphp
 
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                                        @foreach ($keterampilanList as $keterampilan)
+                                                        <li><i class="mdi mdi-check-circle-outline text-success"></i> {{ Str::limit(trim($keterampilan), 7) }}</li>
+                                                        @endforeach
+                                                    </ul>
+                                                </td>
+                                                <td class="text-center text-nowrap align-middle ">
+                                                    <span class="badge bg-success bg-opacity-15 p-1">
+                                                        Rp {{$item->gaji}}
+                                                    </span>
+                                                </td>
+                                                <td class="text-center align-middle">
+                                                    @if($item->gambar_profesi)
+                                                    <div class="d-flex justify-content-center align-items-center" style="border: 2px solid #ddd; padding: 5px; border-radius: 8px; width: 100px; height: 100px; overflow: hidden;">
+                                                        <img src="{{ asset('storage/' . $item->gambar_profesi) }}" alt="Gambar Profesi" style="width: 100%; height: 100%; object-fit: cover;">
+                                                    </div>
+                                                    @else
+                                                    <div class="d-flex justify-content-center align-items-center" style="width: 100px; height: 100px; border: 2px solid #ddd; border-radius: 8px;">
+                                                        <span>Gambar tidak tersedia</span>
+                                                    </div>
+                                                    @endif
+                                                </td>
+
+
+
+
+                                                <td class="text-center">
+                                                    <div class="d-flex justify-content-center align-items-center gap-1" style="min-height: 100px;">
+                                                        <a href="{{ route('profesi.show', $item->id) }}" class="btn btn-warning btn-sm"><i class="ri-eye-fill"></i></a>
+                                                        <a href="{{ route('profesi.edit', $item->id) }}" class="btn btn-info btn-sm">
+                                                            <i class="ri-pencil-fill"></i>
+                                                        </a>
+                                                        <form action="{{ route('profesi.destroy', $item->id) }}" method="POST" style="display:inline;">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?');">
+                                                                <i class="ri-delete-bin-fill"></i>
+                                                            </button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
 
                                 </div>
-                               
+
                             </div> <!-- end card-body -->
                         </div> <!-- end card -->
                     </div> <!-- end col -->
