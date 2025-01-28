73a119c3ff02980c4f47039c60db8d6232163e17
R.Angel <adis.anjeli01@gmail.com>
2025-01-28 17:28:19 +0700

    fix:halaman detail profile admin di hapus

--- resources/views/Layouts/topBar.blade.php
+++ resources/views/Layouts/topBar.blade.php
@@ -3,53 +3,10 @@
             <div class="topbar container-fluid">
                 <div class="d-flex align-items-center gap-1">
 
-                    <!-- Topbar Brand Logo -->
-                    <!-- <div class="logo-topbar"> -->
-                    <!-- Logo light -->
-                    <!-- <a href="index.html" class="logo-light">
-                            <span class="logo-lg">
-                                <img src="assets/images/logo.png" alt="logo">
-                            </span>
-                            <span class="logo-sm">
-                                <img src="assets/images/logo-sm.png" alt="small logo">
-                            </span>
-                        </a> -->
-
-                    <!-- Logo Dark -->
-                    <!-- <a href="index.html" class="logo-dark">
-                            <span class="logo-lg">
-                                <img src="assets/images/logo-dark.png" alt="dark logo">
-                            </span>
-                            <span class="logo-sm">
-                                <img src="assets/images/logo-sm.png" alt="small logo">
-                            </span>
-                        </a>
-                    </div> -->
-
-                    <!-- Sidebar Menu Toggle Button -->
-                    <!-- <button class="button-toggle-menu">
-                        <i class="ri-menu-line"></i>
-                    </button> -->
-
-                    <!-- Horizontal Menu Toggle Button -->
-                    <!-- <button class="navbar-toggle" data-bs-toggle="collapse" data-bs-target="#topnav-menu-content">
-                        <div class="lines">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </button> -->
-
                     <!-- Topbar Search Form -->
                     <div class="app-search d-none d-lg-block mt-1 ms-2">
                         <img src="{{ asset('Assets/images/logo-ars.png') }}" height="21" alt="University Logo" class="mb-1">
                         <strong>ARS UNIVERSITY</strong>
-                        <!-- <form>
-                            <div class="input-group">
-                                <input type="search" class="form-control" placeholder="cari...">
-                                <span class="ri-search-line search-icon text-muted"></span>
-                            </div>
-                        </form> -->
                     </div>
                 </div>
 
@@ -67,12 +24,6 @@
                             </span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated profile-dropdown">
-
-                            <!-- item-->
-                            <a href="{{ url('/profile') }}" class="dropdown-item">
-                                <i class="ri-account-circle-line fs-18 align-middle me-1"></i>
-                                <span>My Account</span>
-                            </a>
 
                             <!-- item-->
                             <a href="#" class="dropdown-item"
